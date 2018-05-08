82bf1a3d4e29921eaa6ac47ef493cfb99387def1
Chris Gioeli <cgioeli@gmail.com>
2018-05-08 17:41:46 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -65,7 +65,7 @@ describe('Objects', function() {
     it('does not modify the original object (it is non-destructive)', function() {
       function deleteFromObjectByKey(object, key){
       var obj = { prop: 1 }
-      delete[object]
+      delete obj[key]
       deleteFromObjectByKey(obj, 'prop')
       expect(obj['prop']).toBe(1)
       }
