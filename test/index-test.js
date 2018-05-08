18b5eee299379f76142567c7cf16f3293d849418
Chris Gioeli <cgioeli@gmail.com>
2018-05-08 16:59:00 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -34,7 +34,9 @@ describe('Objects', function() {
   describe('destructivelyUpdateObjectWithKeyAndValue(object, key, value)', function() {
     it('updates `object` with the given `key` and `value` (it is destructive) and returns the entire updated object', function() {
       var obj = { prop: 1 }
-
+      function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+        
+      }
       expect(destructivelyUpdateObjectWithKeyAndValue(obj, 'prop2', 2)).toMatch({
         prop: 1,
         prop2: 2
