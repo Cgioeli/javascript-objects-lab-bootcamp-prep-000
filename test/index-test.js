86ec0d6a1b2ffff2b9d850f0f62f8dd85d2b72c2
Chris Gioeli <cgioeli@gmail.com>
2018-05-08 17:29:16 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -55,7 +55,7 @@ describe('Objects', function() {
       var obj = { prop: 1 }
       var newObj = deleteFromObjectByKey(obj, 'prop')
       function deleteFromObjectByKey(){
-      Object.assign({},obj)
+      var newObj = Object.assign({},obj)
       delete newObj[obj]
       return newObj
       expect(newObj['prop']).toBe(undefined)
